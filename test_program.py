d568af0d1b2deb57bebc781e28ed3d1af4f93b93
VasanthYennamreddy9 <vasanth@flyfin.org>
2022-07-13 16:43:44 +0530

    commiting all files

---
+++ test_program.py
@@ -0,0 +1,14 @@
+import unittest 
+from  parameterized import parameterized_class
+
+@parameterized_class(('a','b','expected_sum','expected_diff'),(
+    (1, 2, 3, 2),
+   (5, 7, 12, 2),
+))
+
+class TestProgram(unittest.TestCase):
+    def test_add(self):
+      self.assertEqual(self.a + self.b, self.expected_sum)
+
+    def test_sub(self):
+      self.assertEqual(self.a * self.b, self.expected_diff)
